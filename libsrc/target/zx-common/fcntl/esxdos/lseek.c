557ca7dc37785aa2412ccd3b83285bc5a84ff404
Paul Humphreys <querycsv@gmail.com>
2020-06-01 03:10:51 +0100

    ensure stack remains balanced when calling lseek for esxdos

--- libsrc/target/zx-common/fcntl/esxdos/lseek.c
+++ libsrc/target/zx-common/fcntl/esxdos/lseek.c
@@ -5,14 +5,14 @@ long lseek(int fd,long posn, int whence) __naked
 {
 #asm
      EXTERN asm_esxdos_f_seek
-     push   ix	;save users
-     ld     ix,2
+     ;push   ix	;save users
+     ld     ix,4
      add    ix,sp
      ld     l,(ix+0)
      ld     e,(ix+2)
      ld     d,(ix+3)
      ld     c,(ix+4)
-     ld     d,(ix+5)
+     ld     b,(ix+5)
      ld     a,(ix+6)
      call   asm_esxdos_f_seek
      pop    ix
